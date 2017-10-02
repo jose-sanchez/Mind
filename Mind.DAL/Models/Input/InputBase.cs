f8840302602c66f97e7b5ba56658dda6413f7913
jose <jose.sanchez.simon@gmail.com>
2017-10-02 19:29:35 +0200

    Create InputBase

---
+++ Mind.DAL/Models/Input/InputBase.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mind.DAL.Models.Input
+{
+    public abstract class InputBase
+    {
+    }
+}
