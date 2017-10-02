511a638db0e8277a8122888c0134f044aeec01d3
jose <jose.sanchez.simon@gmail.com>
2017-10-02 20:23:31 +0200

    Partial implementation of VisionInput

--- Mind/InputHandlers/VisionInput.cs
+++ Mind/InputHandlers/VisionInput.cs
@@ -8,5 +8,48 @@ namespace Mind.DAL.Models.Input
 {
     public class VisionInput:InputBase
     {
+        private EyeFrame CurrentFrame { get; set; }
+        private EyeFrame PreviousFrame { get; set; }
+        private List<EyeFrame> FrameBuffer { get; set; }
+        public Boolean Recording { get; set; }
+        public void Tick()
+        {   if(Recording && !SomethingChangeInTheFrame())
+            if(SomethingChangeInTheFrame())
+            {
+                StartRecordVisualMemory();
+            }
+                else
+                {
+                    StopRecordVisualMemory();
+                }
+
+            if (Recording)
+            {
+                FrameBuffer.Add(CurrentFrame);
+                PreviousFrame = CurrentFrame;
+            }
+        }
+
+        private void StopRecordVisualMemory()
+        {
+            Recording = false;
+            SendVisualMemoryToProcess();
+            FrameBuffer.Clear();
+        }
+
+        private void SendVisualMemoryToProcess()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void StartRecordVisualMemory()
+        {
+            Recording = true;
+        }
+
+        private bool SomethingChangeInTheFrame()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
