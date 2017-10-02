4b26c266f426bc0bb92ddfecf38ff3607743d21d
jose <jose.sanchez.simon@gmail.com>
2017-10-02 19:40:12 +0200

    create model clases for vision input

---
+++ Mind.DAL/Models/Input/EyeFrame.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mind.DAL.Models.Input
+{
+    public class EyeFrame
+    {
+        public EyeFramePixel[][] eyeFrame;
+
+        public  EyeFrame(int x, int y)
+            {
+            eyeFrame = new EyeFramePixel[x][y];
+            }
+   }
+}
