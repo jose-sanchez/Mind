145a5b3cff963d72823cc2e246d63a5f4ee654a7
jose <jose.sanchez.simon@gmail.com>
2017-10-15 10:37:54 +0200

    Add main priority queues to handle thoughts

---
+++ Mind/PriorityHandler.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mind
+{
+    public class PriorityHandler
+    {
+        //Life on Risk,paint,  priority One
+        Queue<Object> Critical;
+        //Something has to be done soon, waiting for a call , or someone , pooling
+        Queue<Object> Urgent;
+        //Normal level
+        Queue<Object> Normal;
+
+        //Waiting in a room , sleeping,  level
+        Queue<Object> Low;
+
+        //Priory handler moves the processes from other queues to this one. Though Processor procces this queue
+        Queue<Object> Planned;
+
+
+    }
+}
